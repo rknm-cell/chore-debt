3671122f200f77ddfac30e1222ed951ffb0a42e7
rknm-cell <racknahm@gmail.com>
2023-08-23 07:02:43 -0400

    started models.py

--- server/app.py
+++ server/app.py
@@ -3,20 +3,35 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import Flask, request, make_response, jsonify, session
+from flask_cors import CORS
+from flask_migrate import Migrate
 from flask_restful import Resource
+
 
 # Local imports
 from config import app, db, api
 # Add your model imports
+CORS(app)
 
+db.init_app(app)
 
 # Views go here!
 
-@app.route('/')
-def index():
-    return '<h1>Phase 4 Project Server</h1>'
+class Chores(Resource):
+    def get(self):
+        try:
+            chores_dict = [chores.to_dict() for chores in Chore.query.all()]
+            response = make_response(
+                chores_dict,
+                200
+            )
+        except: 
+            response = make_response(
+                404
+            )
 
+api.add_resource(Chores, "/chores")
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,6 +1,30 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.orm import validates
 
 from config import db
 
 # Models go here!
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String, unique = True, nullable = False)
+    _password_hash = db.Column(db.String, nullable = False)
+
+
+class Chore(db.Model):
+    __tablename__ = 'chores'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    description = db.Column(db.String)
+    value = db.Column(db.Integer)
+
+    
+
+class ChoreList(db.Model):
+    __tablename__ = 'chorelists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeigKey('users.id'))
+    chore_id = db.Column(db.Integer, db.ForeignKey('chores.id'))
