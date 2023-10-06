efc415dff186be7b0a755aedefb12fed4d154afd
rknm-cell <racknahm@gmail.com>
2023-10-06 19:27:40 -0400

    added elements to chorecard

--- src/components/ChoreCard.tsx
+++ src/components/ChoreCard.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
 const ChoreCard = () => {
+
   return (
-    <div>ChoreCard</div>
+    <>
+    <h1 className='chore-name'>Chore name</h1>
+    <p className='chore-description'>Chore description</p>
+    <p className='chore-value'>Number</p>
+    </>
   )
 }
 
